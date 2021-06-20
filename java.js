d8136d8d2a0b0f0aa589b2bc8447890debee4b8a
marwanrawshedh <marwansalem55@yahoo.com>
2021-06-20 16:52:09 +0300

    add files

---
+++ java.js
@@ -0,0 +1,10 @@
+//first on local
+let sur= prompt("write your name");
+let age= prompt("welcome " + sur +" in our page  how old are you " + sur +" ?" );
+alert("we will cheack your age "+sur);
+alert("you are "+age+" years old ");
+let home= prompt("where are you from "+sur);
+document.write ('<h4>' + sur+" "+ age + "years old from "+home +'</h4>' );
+  console.log(sur);
+  console.log(age);
+  console.log(home);
